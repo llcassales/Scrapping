5bc54df8de1d7967a4ee656d207ddd06412b7046
llcas <llcassales@gmail.com>
2022-03-24 17:40:15 +0100

    Create a list of links from main website to each authors page

--- ScrapMP.py
+++ ScrapMP.py
@@ -2,12 +2,20 @@ from urllib.request import *
 from bs4 import BeautifulSoup
 import pprint
 
-req = Request('https://www.museodelprado.es/en/the-collection/art-works', headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-soup= BeautifulSoup(webpage, 'html.parser')
-# Find all <a> in your HTML that have a not null 'href'. Keep only 'href'.
-links = [a["href"] for a in soup.find_all("a", href=True)]
-links = [x for x in links if x.startswith('https://www.museodelprado.es/en/the-collection/art-work/')]
-res = []
-[res.append(x) for x in links if x not in res]
-pprint.pprint(res)
+
+artist_links_list =[]
+def artist_links(url = 'https://www.museodelprado.es/en/the-collection'):
+    """This function receives an URL and scraps all the links for artists pages
+
+    """
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, 'html.parser')
+    # Find all <a> in your HTML that have a not null 'href'. Keep only 'href'.
+    links = [a["href"] for a in soup.find_all("a", href=True)]
+    links = [x for x in links if x.startswith('https://www.museodelprado.es/en/the-collection/artist/')]
+    global artist_links_list
+    [artist_links_list.append(x) for x in links if x not in artist_links_list]
+
+artist_links('https://www.museodelprado.es/en/the-collection')
+pprint.pp(artist_links_list)
