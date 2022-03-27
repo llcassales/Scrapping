e2b61c804e74c34cfff4b93e05d7f5afd661b60a
llcas <llcassales@gmail.com>
2022-03-27 13:50:41 +0200

    Get all links for art work pages, we need to increase the number of scrolls in the code to get all( already tested), also drops None values from final list, we need to implement some way to get only art work links ( problably with list comprehension and starts with)

--- Selenium scroll.py
+++ Selenium scroll.py
@@ -11,8 +11,7 @@ time.sleep(1)
 
 elem = browser.find_element_by_tag_name("body")
 
-no_of_pagedowns = 50
-
+no_of_pagedowns = 45
 while no_of_pagedowns:
     elem.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.2)
@@ -26,3 +25,10 @@ print(len(list_links))
 
 list_links = [i for i in list_links if i]
 print(len(list_links))
+
+list_links= [x for x in list_links if x.startswith('https://www.museodelprado.es/coleccion/obra-de-arte/')]
+print(len(list_links))
+
+res = []
+[res.append(x) for x in list_links if x not in res]
+print(len(res))
