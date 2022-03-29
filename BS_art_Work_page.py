1765eb17836902892a40b0b36d78fb7d05f6a67e
llcas <llcassales@gmail.com>
2022-03-29 21:12:02 +0200

    Get all links for art work pages, we need to increase the number of scrolls in the code to get all( already tested), also drops None values from final list, we need to implement some way to get only art work links ( problably with list comprehension and starts with)

--- BS_art_Work_page.py
+++ BS_art_Work_page.py
@@ -4,15 +4,14 @@ import json
 import requests
 
 #Now read the file back into a Python list object
-with open('test.txt', 'r') as f:
+with open('test.txt', 'r') as f: #imports list of art work links created in previous script
     list = json.loads(f.read())
-
-list1 = list[0:10]
-print(list1)
+#pending remove duplicates
+list1 = list[0:10] # small part of the list just to make a sample, otherwise its running time is two long, also this list is duplicated
 df = pd.DataFrame()
 
 for url in list1:
 # def getInfo(URL):
       content = requests.get(url).text
       soup = BeautifulSoup(content, "html.parser")
-      print(soup.find('em'))
+      print(soup.find('em')) #iterates over list to bring name of the art work.
