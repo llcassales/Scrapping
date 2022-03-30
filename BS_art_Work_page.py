b260a2f5ae02ebe9235296098dfbb6a86d17aa12
llcas <llcassales@gmail.com>
2022-03-30 19:49:53 +0200

    Get all links for art work pages, we need to increase the number of scrolls in the code to get all( already tested), also drops None values from final list, we need to implement some way to get only art work links ( problably with list comprehension and starts with)

--- BS_art_Work_page.py
+++ BS_art_Work_page.py
@@ -3,15 +3,25 @@ import pandas as pd
 import json
 import requests
 
-#Now read the file back into a Python list object
-with open('test.txt', 'r') as f: #imports list of art work links created in previous script
-    list = json.loads(f.read())
-#pending remove duplicates
-list1 = list[0:10] # small part of the list just to make a sample, otherwise its running time is two long, also this list is duplicated
-df = pd.DataFrame()
+pd.set_option('display.width', 400)  # Settings for view in Pycharm *ignore
+pd.set_option('display.max_columns', 10)
+
+# Read the file into a Python list object
+with open('test.txt', 'r') as f:  # Imports list of art work links created in previous script
+    list_links = json.loads(f.read())
+
+list_links = [x for x in list_links if "?search" not in x]  # Eliminates duplicated links
+
+
+list1 = list_links[0:10]  # Small part of the list just to make a sample, otherwise its running time is two long, also this list is duplicated
+df = pd.DataFrame(columns=['Titles', 'Date', 'Author'])
 
 for url in list1:
-# def getInfo(URL):
-      content = requests.get(url).text
-      soup = BeautifulSoup(content, "html.parser")
-      print(soup.find('em')) #iterates over list to bring name of the art work.
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, "html.parser")
+    new_row = {'Titles': soup.find('em').text, 'Date': soup.find('dd', attrs={'property': 'date:textDate'}),
+               'Author': soup.find('span', attrs={'property': 'ecidoc:p131_E82_p102_has_title'})}  # Iterates over list to bring name of the art work.
+    df = df.append(new_row, ignore_index=True)
+df.drop_duplicates()
+print(df)
+df.to_csv(index=False)
--- Selenium scroll.py
+++ Selenium scroll.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -14,29 +15,29 @@ time.sleep(1)
 elem = browser.find_element_by_tag_name("body")
 
 no_of_pagedowns = 150
-while no_of_pagedowns:
+while no_of_pagedowns:  # Loops using page downs to scroll all the page and load all html code
     elem.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.2)
     no_of_pagedowns-=1
 
 post_elems = browser.find_elements_by_tag_name("a")
 list_links = []
-for post in post_elems:
+for post in post_elems:  # Append links in the page to this list
     list_links.append(post.get_attribute('href'))
 print(len(list_links))
 
-list_links = [i for i in list_links if i]
+list_links = [i for i in list_links if i]  # Remove None values
 print(len(list_links))
 
-list_links = [x for x in list_links if x.startswith('https://www.museodelprado.es/coleccion/obra-de-arte/')]
+list_links = [x for x in list_links if x.startswith('https://www.museodelprado.es/coleccion/obra-de-arte/')]  # Get only art work links
 print(len(list_links))
 
 res = []
-[res.append(x) for x in list_links if x not in res]
+[res.append(x) for x in list_links if x not in res]  # Remove Duplicates
 print(len(res))
 pprint.pp(res)
 
 
-with open('test.txt', 'w') as f:
+with open('test.txt', 'w') as f:  # Save in txt file
     f.write(json.dumps(res))
 
